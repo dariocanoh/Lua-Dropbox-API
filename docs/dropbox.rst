de80058d61a8c054e5c52dd376f2e591e07e31b8
dcanoh <thdkano@gmail.com>
2017-07-02 11:51:29 -0500

    Updated library

--- docs/dropbox.rst
+++ docs/dropbox.rst
@@ -27,14 +27,48 @@ Cambiando "<app_key>" por la clave que obtenemos en la consola de dropbox
 
 ----------------------------------------------------------------------------------------------------
 
+dropbox.auth_token_revoke
+^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Deshabilita el token de acceso usado para autenticar las llamadas.
+   
+==========  ========================================================================================
+  bool_      dropbox.auth_token_revoke ( string_ 'access_token' )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
 dropbox.get_account_info
 ^^^^^^^^^^^^^^^^^^^^^^^^
    
 Devuelve la informacion de la cuenta de Dropbox en una tabla.
    
 ==========  ========================================================================================
-  table     dropbox.get_account_info ( string_ 'access_token' )
+  table      dropbox.get_account_info ( string_ 'access_token' )
 ==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.get_space_usage
+^^^^^^^^^^^^^^^^^^^^^^^  
+
+Devuelve la información acerca del espacio utilizado para la cuenta actual. 
+
+**(Los valores están expresados en bytes).**
+
+==========  ========================================================================================
+  table      dropbox.get_space_usage ( string_ 'access_token' )
+==========  ========================================================================================
+
+.. code-block::
+  
+ {
+    "used": 314159265,
+    "allocation": {
+        ".tag": "individual",
+        "allocated": 10000000000
+    }
+ }
 
 ----------------------------------------------------------------------------------------------------
 
@@ -44,5 +78,73 @@ dropbox.get_file_content
 Obtiene el contenido de un archivo binario o de texto.
 
 =========  ===========================================================================================
- string  	dropbox.get_file ( string_ 'access_token', string_ 'dropbox_file_path' )
+ string_  	dropbox.get_file ( string_ 'access_token', string_ 'dropbox_file_path' )
 =========  ===========================================================================================
+
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.create_folder
+^^^^^^^^^^^^^^^^^^^^^  
+
+Crear una carpeta en la ruta especificada.
+
+.. code-block::
+
+ - path: (String pattern = "(/(.|[\r\n])*)|(ns:[0-9]+(/.*)?)") La ruta a crear dentro del dropbox.
+ - autorename: Si hay un conflicto, el servidor de dropbox tiene qué renombrar la carpeta para 
+   resolver el conflicto.
+
+==========  ========================================================================================
+  table_     dropbox.create_folder ( string_ 'path', bool_ autorename = false )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.file_delete
+^^^^^^^^^^^^^^^^^^^  
+
+Eliminar el archivo en la ruta especificada.
+
+.. code-block::
+
+ - path: (String pattern = "(/(.|[\r\n])*)|(ns:[0-9]+(/.*)?)") La ruta a crear dentro del dropbox.
+
+==========  ========================================================================================
+  table_     dropbox.file_delete ( string_ 'access_token', string_ 'path' )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.file_download
+^^^^^^^^^^^^^^^^^^^^^  
+
+Descargar el archivo desde Dropbox a la ruta especificada.
+
+==========  ========================================================================================
+  table_     dropbox.file_delete ( string_ 'access_token', string_ 'path', string_ 'destination' )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.file_upload
+^^^^^^^^^^^^^^^^^^^
+
+Descargar el archivo desde Dropbox a la ruta especificada.
+
+==========  ========================================================================================
+  table_     dropbox.file_upload ( string_ access_token, string_ file_to_upload, string_ path, string_ mode, bool_ autorename = false, bool_ mute = false  )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
+
+dropbox.get_current_account
+^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+
+Obtiene la información acerca de la cuenta de usuario actual.
+
+==========  ========================================================================================
+  table_     dropbox.get_current_account ( string_ 'access_token' )
+==========  ========================================================================================
+
+----------------------------------------------------------------------------------------------------
--- init.lua
+++ init.lua
@@ -1,21 +1,51 @@
 -- /////////////////////////////////////////////////////////////////////////////////////////////////
 -- // Name:        luadropboxapi/init.lua
--- // Purpose:     Initialize Lua Dropbox API functions
--- // Author:      Dario Cano [thdkano@gmail.com]
+-- // Purpose:     Lua Dropbox API functions
+-- // Author:      Hernan Dario Cano [dcanohdev@gmail.com]
 -- // Created:     2016/09/15
--- // Copyright:   (c) 2016 Dario Cano
--- // License:     MIT License
+-- // Copyright:   (c) 2016 Hernan Dario Cano
+-- // License:     MIT/X11
 -- /////////////////////////////////////////////////////////////////////////////////////////////////
 --
---- dropbox.get_access_token
---- dropbox.get_account_info
---- dropbox.get_file_content
----
+-- All dates in the API are strings in the following format:
+-- "Sat, 21 Aug 2010 22:31:20 +0000"
+-- In code format, which can be used in all programming languages that support strftime or strptime:
+-- "%a, %d %b %Y %H:%M:%S %z"
+
+--[[ status_codes:
+
+400	Bad input parameter. Error message should indicate which one and why.
+401	Bad or expired token. This can happen if the user or Dropbox revoked or expired an access token. To fix, you should re-authenticate the user.
+403	Bad OAuth request (wrong consumer key, bad nonce, expired timestamp...). Unfortunately, re-authenticating the user won't help here.
+404	File or folder not found at the specified path.
+405	Request method not expected (generally should be GET or POST).
+429	Your app is making too many requests and is being rate limited. 429s can trigger on a per-app or per-user basis.
+503	If the response includes the Retry-After header, this means your OAuth 1.0 app is being rate limited. Otherwise, this indicates a transient server error, and your app should retry its request.
+507	User is over Dropbox storage quota.
+5xx	Server error. Check DropboxOps.
+
+]]
+
+function print ( txt )
+	Dialog.Message('DROPBOXAPI:', tostring(txt))
+end
+
+local function validate_response ( response )
+	local DROPBOX_BAD_REQUEST       = 400
+	local DROPBOX_BAD_EXPIRED_TOKEN = 401
+	
+	if (response.status_code == DROPBOX_BAD_REQUEST) 
+	or (response.status_code == DROPBOX_BAD_EXPIRED_TOKEN) then
+		return false, response.text
+	else
+		return response.json()
+	end
+end
 
 local requests = require 'requests'
 local dropbox  = {}
 
-dropbox.acces_token = nil
+dropbox.access_token = nil
 
 --- Devuelve el token de acceso que permite hacer operaciones con la api de dropbox.
 --- client_id y client_secret son appkey y appsecret respectivamente, estos los podemos encontrar en:
@@ -28,41 +58,128 @@ dropbox.acces_token = nil
 ---
 --- string access_token = dropbox.get_access_token ( string 'app_key', string 'app_secret', string 'auth_code' );
 ---
-function dropbox.get_access_token ( client_id, client_secret, auth_code )
+function dropbox.get_access_token ( client_id, client_secret, auth_code, redirect_uri )
 	local query_parameters  = { 
-		grant_type    = 'authorization_code', 
-		code          = (auth_code),
-		client_id     = client_id,
-		client_secret = client_secret,
+		grant_type = 'authorization_code', 
+		code       = auth_code,
+		--client_id     = client_id,
+		--client_secret = client_secret,
+		redirect_uri  = redirect_uri,
 	}
-	
-	local response = requests.post { url = 'https://api.dropboxapi.com/1/oauth2/token', params = query_parameters }
 
-	if response.json().error then
-		print ('http error:', response.json().error, response.json().error_description)
+	local auth = requests.HTTPBasicAuth(client_id, client_secret)
+	local response = requests.post { url = 'https://api.dropbox.com/1/oauth2/token', auth = auth, params = query_parameters }
+	
+	if response.status_code == 400 then
+		print('dropbox.lua:' .. response.text)
+		return false, response.text
 	end
 
-	for json_key, json_val in pairs( response.json() ) do
-		if (json_key == 'access_token') then
-			return json_val
-		end
+	if response.json() then
+		dropbox.access_token = response.json().access_token--, response.json().account_id
+		return dropbox.access_token
 	end
 end
 
---- Devuelve la informacion de la cuenta de Dropbox en una tabla de acuerdo al documento json original.
---- 
---- table account_info = dropbox.get_account_info ( string 'access_token' )
----
-function dropbox.get_account_info ( access_token )
-	local query_parameters = { locale = 'en' }
-
+-- Disables the access token used to authenticate the call.
+function dropbox.auth_token_revoke ( access_token )
 	local headers = {
-		['Authorization'] = 'Bearer ' .. access_token
+		['Authorization'] = 'Bearer ' .. access_token,
 	}
 
-	local response = requests.get {url = 'https://api.dropboxapi.com/1/account/info', headers = headers, query_parameters = query_parameters }
+	local response = requests.post { url = 'https://api.dropboxapi.com/2/auth/token/revoke', headers = headers }
+	
+	if response.status_code == 400 then
+		return false, response.text
+	end
 
-	return response.json()	
+	return true
+end
+
+-- Get the space usage information for the current user's account.
+function dropbox.get_space_usage ( access_token )
+	local headers = {
+		['Authorization'] = 'Bearer ' .. access_token,
+	}
+
+	local response = requests.post { url = 'https://api.dropboxapi.com/2/users/get_space_usage', headers = headers }
+	
+	if response.status_code == 400 then
+		return false, response.text
+	end
+
+	return response.json()
+end
+
+
+function dropbox.list_folder( access_token, path, recursive, include_media_info, include_deleted, include_has_explicit_shared_members )
+	local data = { 
+		path      = tostring(path),
+		recursive = recursive,
+		include_media_info = include_media_info,
+		include_deleted = include_deleted,
+		include_has_explicit_shared_members = include_has_explicit_shared_members,
+	}
+
+	local headers = {
+		['Authorization'] = 'Bearer ' .. access_token,
+		['Content-Type']  = 'application/json',
+	}
+
+	local response = requests.post { url = 'https://api.dropboxapi.com/2/files/list_folder', headers = headers, data = data }	
+	
+    return validate_response(response)
+end
+
+-- Create a folder at a given path.
+-- metadata = dropbox.create_folder ( token, '/thepath' )
+function dropbox.create_folder ( access_token, path, autorename )
+	local headers = {
+		['Authorization'] = 'Bearer ' .. access_token,
+		['Content-Type']  = 'application/json',
+	}
+	
+	local query_parameters = {
+		path = path,
+		autorename = autorename,
+	}
+
+	local response = requests.post { 
+		url = 'https://api.dropboxapi.com/2/files/create_folder_v2',
+		headers = headers, data = query_parameters 
+	}
+
+	if validate_response(response) then
+		return validate_response(response).metadata
+	else
+		return validate_response(response)
+	end
+end
+
+-- Delete the file or folder at a given path.
+-- If the path is a folder, all its contents will be deleted too.
+-- A successful response indicates that the file or folder was deleted. The returned metadata will be the corresponding FileMetadata or FolderMetadata for the item at time of deletion, and not a DeletedMetadata object.
+
+function dropbox.file_delete ( access_token, path )
+	local headers = {
+		['Authorization'] = 'Bearer ' .. access_token,
+		['Content-Type']  = 'application/json',
+	}
+	
+	local query_parameters = {
+		path = path,
+	}
+
+	local response = requests.post { 
+		url = 'https://api.dropboxapi.com/2/files/delete_v2',
+		headers = headers, data = query_parameters 
+	}
+	
+	if validate_response(response) then
+		return validate_response(response).metadata
+	else
+		return validate_response(response)
+	end
 end
 
 --- Devuelve el contenido del archivo o false si hubo un error.
@@ -92,35 +209,57 @@ function dropbox.get_file_content ( access_token, cloud_file_path )
 	end
 end
 
-function dropbox.list_folder( path, recursive, include_media_info, include_deleted, include_has_explicit_shared_members )
-	local data = { 
-		path      = tostring(path),
-		recursive = recursive,
-		include_media_info = include_media_info,
-		include_deleted = include_deleted,
-		include_has_explicit_shared_members = include_has_explicit_shared_members,
-	}
+-- Download a file from a user's Dropbox.
+-- %to.do% upgrade endpoints to api v2: https://content.dropboxapi.com/2/files/download
+function dropbox.file_download( access_token, path, destination)
+	local dest_file = io.open (destination, 'w+b')
+	local content   = dropbox.get_file_content ( access_token, path )
 
-	local headers = {
-		['Authorization'] = 'Bearer ' .. dropbox.access_token,
-		['Content-Type']  = 'application/json',
-	}
+	dest_file:write(content)
+	dest_file:flush()
+	dest_file:close()
 
-	local response = requests.post { url = 'https://api.dropboxapi.com/2/files/list_folder', headers = headers, data = data }	
-	
-	--400	Bad input parameter. The response body is a plaintext message with more information.
-	if (response.status_code == 400) then
-		-- Error in call to API function "files/list_folder": request body: path: '/filedasdsxa' did not match pattern '(/(.|[\r\n])*)?|(ns:[0-9]+(/.*)?)'
-		lide_last_error = response.text
-		return false, tostring(response.text or 'status 400')
+	local read = io.open(destination, 'r+b');
+	if read:read '*a' == content then
+		read:close()
+		return true
+	else
+		return false
 	end
+end
+
+local json = require 'cjson'
+
+-- Create a new file with the contents provided in the request.
+-- Do not use this to upload a file larger than 150 MB. Instead, create an upload session with upload_session/start.
+function dropbox.file_upload ( access_token, file_to_upload, path, mode, autorename, mute )
+	local headers = {
+		['Authorization']   = 'Bearer ' .. access_token,
+		['Content-Type']    = 'application/octet-stream',
+		['Dropbox-API-Arg'] = json.encode { path = path, mode = mode, autorename, autorename, mute, mute }
+	}
 	
-	--path_display	/Public/Ejemplos
-	--path_lower	/public/ejemplos
-	--name	Ejemplos
-	--id	id:In5FPScmX0QAAAAAAAABxA
-	--.tag	folder
-	return response.json().entries
+	local response = requests.post { 
+		url = 'https://content.dropboxapi.com/2/files/upload',
+		headers = headers, --arg = json.encode(query_data),
+		
+		['data-binary'] = io.open(file_to_upload, 'r+b'):read '*a'
+	}
+	
+	if validate_response(response) then
+		return validate_response(response)
+	end
+end
+
+--- Get information about the current user's account.
+function dropbox.get_current_account ( access_token )
+	local headers = {
+		['Authorization'] = 'Bearer ' .. access_token
+	}
+
+	local response = requests.get {url = 'https://api.dropboxapi.com/2/users/get_current_account', headers = headers }
+
+	return response.json()
 end
 
 return dropbox
